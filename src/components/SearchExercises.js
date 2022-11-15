29dc374b62a146b1ac2ca80fc7fe1afcef383f44
Aditya Singh <aditya7068singh@gmail.com>
2022-11-15 22:07:57 +0530

    exercise card

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,9 +3,9 @@ import {Box,Stack,TextField,Typography,Button} from '@mui/material'
 import { exerciseOptions,fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar'
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises,bodyPart,setBodyPart}) => {
   const[search,setSearch]=useState("");
-  const[exercises,setExercises]=useState([]);
+ 
   const[bodyParts,setBodyParts]=useState([]);
 
   useEffect (()=> {
@@ -69,7 +69,8 @@ const SearchExercises = () => {
         >Search</Button>
       </Box>
       <Box sx={{position: 'relative',width: '100%',p:'20px'}}>
-         <HorizontalScrollbar data={bodyParts}/>
+         <HorizontalScrollbar data={bodyParts}  bodyPart={bodyPart} 
+       setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
