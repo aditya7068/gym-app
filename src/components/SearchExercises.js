ee486a5d4fb69ebce4a0533c18b81184438c5ed1
Aditya Singh <aditya7068singh@gmail.com>
2023-07-06 06:03:18 +0530

    api change

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -24,7 +24,7 @@ const SearchExercises = ({setExercises,bodyPart,setBodyPart}) => {
   const handleSearch=async () => {
   if(search){
     const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises',exerciseOptions);
-   console.log(exercisesData);
+  //  console.log(exercisesData);
     const searchedExercises=exercisesData.filter(
       (exercise) => exercise.name.toLowerCase().includes(search)
       ||  exercise.target.toLowerCase().includes(search)
